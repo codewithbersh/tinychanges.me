ce5d1a5f7b5b69393c24e062610fa4564010f86a
codewithbersh <121822380+codewithbersh@users.noreply.github.com>
2023-11-04 12:54:51 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 const Home = () => {
   return (
     <div className="grid place-items-center h-full">
-      <h1 className="text-2xl font-extrabold">tinychanges.me</h1>
+      <div className="space-y-6 text-center">
+        <h1 className="text-2xl font-medium">tinychanges.me</h1>
+        <Link
+          href="https://twitter.com/codewithbersh"
+          target="_blank"
+          className="text-muted-foreground text-sm underline"
+        >
+          codewithbersh
+        </Link>
+      </div>
     </div>
   );
 };
