19d52bc5c646a47552897368fe3f99902ec1e6ad
codewithbersh <121822380+codewithbersh@users.noreply.github.com>
2023-11-04 17:14:07 +0300

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "@/styles/globals.css";
 import { GeistSans } from "geist/font";
+import { Analytics } from "@vercel/analytics/react";
 
 import { Providers } from "@/components/providers/providers";
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={GeistSans.className}>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
--- trpc/routers/waitlist.ts
+++ trpc/routers/waitlist.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { publicProcedure, router } from "../trpc";
+import { publicProcedure, router } from "@/trpc/trpc";
 import db from "@/lib/prismadb";
 
 export const waitlistRouter = router({
