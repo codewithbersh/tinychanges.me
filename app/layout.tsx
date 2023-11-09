fae56068c988510d2b5309aed6ffc302f69ceefe
codewithbersh <121822380+codewithbersh@users.noreply.github.com>
2023-11-09 11:18:11 +0300

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,27 @@ import "@/styles/globals.css";
 import { GeistSans } from "geist/font";
 import { Analytics } from "@vercel/analytics/react";
 import { Toaster } from "sonner";
+import { siteConfig } from "@/config/site";
 
 import { Providers } from "@/components/providers/providers";
 
 export const metadata: Metadata = {
-  title: "Tiny Changes",
-  description: "Tiny Changes, Remarkable Results.",
+  title: siteConfig.title,
+  description: siteConfig.url,
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/favicon-light.ico",
+        href: "/favicon-light.ico",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/favicon-dark.ico",
+        href: "/favicon-dark.ico",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  title: "Tiny Changes",
+  description: "A daily habit tracker.",
+  url: "https://www.tinychanges.me",
+};
