157a66007df6e357af076a9cd8ac41efdf6409d7
codewithbersh <121822380+codewithbersh@users.noreply.github.com>
2023-11-21 13:06:45 +0300

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 import { PropsWithChildren } from "react";
 
 const AuthLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="h-full w-full grid place-items-center">{children}</div>
+    <div className="relative grid h-full w-full place-items-center">
+      <Button
+        asChild
+        variant="ghost"
+        className="absolute left-4 top-4 h-auto px-4 py-2 md:left-8 md:top-8"
+      >
+        <Link href="/">
+          <ChevronLeft className="mr-2 h-4 w-4" /> Back
+        </Link>
+      </Button>
+      <div>{children}</div>
+    </div>
   );
 };
 
