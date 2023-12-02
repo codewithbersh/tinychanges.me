df96cead9c5c40d56c40f0f9cfae30883f1d0246
codewithbersh <121822380+codewithbersh@users.noreply.github.com>
2023-12-02 15:07:44 +0300

    fix: colors undefined

--- config/habit.ts
+++ config/habit.ts
@@ -1,4 +1,30 @@
 export const habitConfig = {
+  colors: [
+    {
+      id: "green-600",
+      hex: "#22c55e",
+    },
+    {
+      id: "red-600",
+      hex: "#dc2626",
+    },
+    {
+      id: "yellow-500",
+      hex: "#eab308",
+    },
+    {
+      id: "blue-500",
+      hex: "#3b82f6",
+    },
+    {
+      id: "purple-600",
+      hex: "#9333ea",
+    },
+    {
+      id: "pink-600",
+      hex: "#db2777",
+    },
+  ],
   defaultAvatarImages: [
     {
       src: "/avatars/boy-blue.png",
