38a1c47b8bf4cf88fbdda19b21c19cd991e83df9
codewithbersh <121822380+codewithbersh@users.noreply.github.com>
2023-12-06 10:49:38 +0300

    fix: only auth user can go to profile

--- app/(main)/[slug]/(routes)/profile/page.tsx
+++ app/(main)/[slug]/(routes)/profile/page.tsx
@@ -1,6 +1,19 @@
+import { notFound } from "next/navigation";
+import { getCurrentUser } from "@/lib/get-current-user";
+
 import { Form } from "./_components/form";
 
-const ProfilePage = () => {
+interface ProfilePageProps {
+  params: {
+    slug: string;
+  };
+}
+
+const ProfilePage = async ({ params: { slug } }: ProfilePageProps) => {
+  const user = await getCurrentUser();
+  if (!user || user.slug !== slug) {
+    notFound();
+  }
   return <Form />;
 };
 
